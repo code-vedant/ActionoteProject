0789eb1c20d30298a60a5fb6a5c2aefd01075318
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-01-24 19:21:53 +0530

    shifting

--- Backend/src/routes/pomodoro.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createPomodoro } from '../controllers/pomodoro.controller.js';
-
-const router = express.Router();
-
-// Route to create a new Pomodoro session
-router.post('/', createPomodoro);
-
-export default router;
